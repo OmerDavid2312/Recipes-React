174601f2921515ee567d27071d50a102ee5b27f8
Omer <omerdavid97@gmail.com>
2020-05-13 15:31:45 +0300

    Updates

---
+++ precache-manifest.fe9e7381a9eaae4534f0fd30ffb1273f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18c429473494bdbf63ce4b89fe713ed8",
+    "url": "/Recipe-App-React/index.html"
+  },
+  {
+    "revision": "0a7cacbf6a027b046e6f",
+    "url": "/Recipe-App-React/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "3f286cfc6daf7aeefc26",
+    "url": "/Recipe-App-React/static/css/main.4a0c93fd.chunk.css"
+  },
+  {
+    "revision": "0a7cacbf6a027b046e6f",
+    "url": "/Recipe-App-React/static/js/2.ceb75398.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Recipe-App-React/static/js/2.ceb75398.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3f286cfc6daf7aeefc26",
+    "url": "/Recipe-App-React/static/js/main.b68a96ce.chunk.js"
+  },
+  {
+    "revision": "417e238332b827713133",
+    "url": "/Recipe-App-React/static/js/runtime-main.a3885ed9.js"
+  },
+  {
+    "revision": "926574699e46d61553351127af9930f5",
+    "url": "/Recipe-App-React/static/media/photo-1466637574441-749b8f19452f.92657469.jpg"
+  }
+]);
